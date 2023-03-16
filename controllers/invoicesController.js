e5d5ab64db475fcbffd50d2d05df7138cdd8215a
DizzlyD <david4henery00@hotmail.com>
2023-03-16 14:11:02 +0000

    setup-delete-&-create-invoices-new-&drafts

--- controllers/invoicesController.js
+++ controllers/invoicesController.js
@@ -74,16 +74,20 @@ const editInvoice = asyncHandler(async (req, res) => {
 // delete /invoices
 // access: private
 const deleteInvoice = asyncHandler(async (req, res) => {
-  const {id} = req.body
-  if(!id){
+  const {_id, userId} = req.body
+  if(!_id){
     return res.status(400).json({message: "Need Invoice id"})
   }
   
-  const invoice = await Invoice.findOne({id}).exec()
+  const invoice = await Invoice.findOne({_id}).exec()
   if (!invoice) return res.status(400).json({message: "Invoice not found"})
-  
+
   const result = await invoice.deleteOne()
-  res.json({message: `Invoice ${result.id} deleted`})
+
+  // NEW INVOICE LIST TO RETURN
+  const allInvoices = await Invoice.find({ userId }).lean();
+  
+  res.json({message: `Invoice deleted`, invoices: allInvoices})
 });
 
 module.exports = {
